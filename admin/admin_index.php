93c377ecf673cec1f1e292603e9fd5c8f227cd92
Alisha Anand <alishaanand86.aa@gmail.com>
2020-08-02 02:13:39 +0530

    Add files via upload

---
+++ admin/admin_index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Admin Panel</title>
+		<link rel="stylesheet" href="admin_style.css" />
+	</head>
+	
+	<body>
+			<div id="header"></div><!--end of header-->
+	</body>
+	
+</html>
